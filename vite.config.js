99bff62f160252f54aa34f188f6ce98c4828f721
tsuzuki <goodin557@gmail.com>
2023-01-12 10:27:08 +0900

    set

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        index01: resolve(__dirname, 'index01.html'),
+        index02: resolve(__dirname, 'index02.html'),
+      },
+    },
+  },
 })
